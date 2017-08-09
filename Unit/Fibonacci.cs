b242d7cd87b822a0a292be29b6fb9714d369933a
JuliaKoe <yulianakoeberle@gmail.com>
2017-08-09 14:36:31 -0400

    renamted Fib class methods

--- Unit/Fibonacci.cs
+++ Unit/Fibonacci.cs
@@ -8,7 +8,7 @@ using NUnit.Framework;
 namespace Unit
 {
     /// <summary>
-    /// Calculates n-th Fib number recursively and linearly
+    /// Calculate Fib number linearly and recursively
     /// </summary>
     public class Fibonacci
     {
@@ -17,13 +17,13 @@ namespace Unit
         /// </summary>
         /// <param name="num"></param>
         /// <returns>n-th number in Fib sequence</returns>
-        public int fibRecursive(int num)
+        public int GetFibNumber(int num)
         {
             //base case
             if (num == 0) return 0;
             else if (num == 1) return 1;
 
-            return (fibRecursive(num - 1) + fibRecursive(num - 2));
+            return (GetFibNumber(num - 1) + GetFibNumber(num - 2));
         }
 
         /// <summary>
@@ -31,7 +31,7 @@ namespace Unit
         /// </summary>
         /// <param name="num"></param>
         /// <returns>n-th number in Fib sequence</returns>
-        public int calcFibLinear(int num)
+        public int GetFibNumber(int num, bool linear)
         {
             //0 1 1 2 3 5 8 ...
             int a = 0, b = 1, prev = 0;
@@ -64,9 +64,9 @@ namespace Unit
         [TestCase(1,1)]
         [TestCase(3,2)]
         [TestCase(5,5)]
-        public void ItShouldReturnCorrectValue(int num, int fib)
+        public void ItShouldCalcFibRecursive(int num, int fib)
         {
-            int result = f.fibRecursive(num);
+            int result = f.GetFibNumber(num);
 
             Assert.That(result, Is.EqualTo(fib));
         }
@@ -77,7 +77,7 @@ namespace Unit
         [TestCase(5, 5)]
         public void ItShouldCalcFibLinear(int num, int fib)
         {
-            int result = f.calcFibLinear(num);
+            int result = f.GetFibNumber(num, true);
 
             Assert.That(result, Is.EqualTo(fib));
         }
